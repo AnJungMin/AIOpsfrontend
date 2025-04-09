a8242dac0601ef74192549bdc78bc96ada702f8c
AnJungMin <dkswjdals4027@gmail.com>
2025-04-09 15:57:22 +0900

    Vite alias (@) 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 👈 이거 추가!
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 👈 이 줄 추가!
+    },
+  },
 })
