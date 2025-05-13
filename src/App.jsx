a2b2588e46f0541fe191300b2284868faeabe885
AnJungMin <dkswjdals4027@gmail.com>
2025-05-13 16:25:15 +0900

    App.jsx fix2

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Home from "./pages/Home";
 import Diagnosis from "./pages/DiagnosisPage";
 import Result from "./pages/Result";
 import ProductPage from "./pages/ProductPage";
-import CommunityPage from "./pages/CommunityPage";
-import LoginPage from "./pages/LoginPage";
 import ProductList from "./pages/ProductList";
 import ProductDetail from "./pages/ProductDetail";
 
