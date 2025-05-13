e38d3ea26d79e06cd6157628232005b72349ee46
AnJungMin <dkswjdals4027@gmail.com>
2025-05-13 16:23:46 +0900

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/LandingPage";
+import Home from "./pages/Home";
 import Diagnosis from "./pages/DiagnosisPage";
 import Result from "./pages/Result";
 import ProductPage from "./pages/ProductPage";
